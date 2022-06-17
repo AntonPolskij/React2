6118f4cb945f269beff763aca9095eb7e3169308
Anton Polskiy <polskij.anton707@gmail.com>
2022-06-17 04:03:09 +0300

    Revert "lesson-3 (нужно доработать стили сообщений и добавить тему)"
    This reverts commit 2b512351a2b086ead750f133d67ad6297ab52bdc.

--- react2/src/components/Message/Message.jsx
+++ react2/src/components/Message/Message.jsx
@@ -1,11 +1,10 @@
-// import classnames from 'classnames';
-// import styles from './message.module.css';
-import { Paper } from '@mui/material';
+import classnames from 'classnames';
+import styles from './message.module.css';
 
-export const Message = ({ text, author, ...props }) => {
-    // let className = classnames(styles.msg, {
-    //     [styles.me]: props.theme === 'Me',
-    //     [styles.from]: props.theme === 'Bot',
-    // })
-    return <Paper variant='outlined' component='li'>{author}: {text}</Paper>
+export const Message = ({text, author, ...props}) => {
+    let className = classnames(styles.msg, {
+        [styles.me]: props.theme === 'Me',
+        [styles.from]: props.theme === 'Bot',
+    })
+    return <div className={className}>{author}: {text}</div>
 }
