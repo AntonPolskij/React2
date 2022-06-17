2b512351a2b086ead750f133d67ad6297ab52bdc
Anton Polskiy <polskij.anton707@gmail.com>
2022-06-17 03:57:02 +0300

    lesson-3 (нужно доработать стили сообщений и добавить тему)

--- react2/src/components/Message/Message.jsx
+++ react2/src/components/Message/Message.jsx
@@ -1,10 +1,11 @@
-import classnames from 'classnames';
-import styles from './message.module.css';
+// import classnames from 'classnames';
+// import styles from './message.module.css';
+import { Paper } from '@mui/material';
 
-export const Message = ({text, author, ...props}) => {
-    let className = classnames(styles.msg, {
-        [styles.me]: props.theme === 'Me',
-        [styles.from]: props.theme === 'Bot',
-    })
-    return <div className={className}>{author}: {text}</div>
+export const Message = ({ text, author, ...props }) => {
+    // let className = classnames(styles.msg, {
+    //     [styles.me]: props.theme === 'Me',
+    //     [styles.from]: props.theme === 'Bot',
+    // })
+    return <Paper variant='outlined' component='li'>{author}: {text}</Paper>
 }
